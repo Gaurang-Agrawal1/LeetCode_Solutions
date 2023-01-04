02feff3fb9aa411af6079cedfb272a0373612d4a
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-01-04 08:53:07 +0530

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -29,4 +29,20 @@ Constraints:
   
   Solution-->>
   
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int []a=new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]+nums[i]==target){
+                    a[0]=i;
+                    a[1]=j;
+                    break;
+                }
+            }
+            }
+        return a;
+        }
+    }
+  
   
