2d1f2ddfe0ea97de4d49ce0d4a7f5750fd6dca5f
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-05-25 07:55:54 +0530

    Update No_of_steps_to_reduce_a_no_to_zero.java

--- No_of_steps_to_reduce_a_no_to_zero.java
+++ No_of_steps_to_reduce_a_no_to_zero.java
@@ -41,4 +41,20 @@ Constraints:
 Solution-
   
  
-
+class Solution {
+    public int numberOfSteps(int num) {
+        int c=0;
+       while(num!=0){ 
+           if(num%2==0){
+              c++;
+              num=num/2;
+              
+        }
+        else{
+            num=num-1;
+            c++;
+        }
+    }
+        return c;
+    }
+}
