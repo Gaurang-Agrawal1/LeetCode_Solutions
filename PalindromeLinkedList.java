d66e462a28a66d7df8e99768d3190ca159a8aedb
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-06-20 22:29:17 +0530

    Update PalindromeLinkedList.java

--- PalindromeLinkedList.java
+++ PalindromeLinkedList.java
@@ -24,5 +24,58 @@ Constraints:
 
 Solution-
 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode curr = slow;
+        ListNode prev = null;
+        ListNode next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode head1 = head;
+        ListNode head2 = prev;
+
+        while(head2 != null && head1 != null){
+            if(head1.val != head2.val){
+                return false;
+            }
+
+            head1 = head1.next;
+            head2 = head2.next; 
+        }
+
+        return true;
+
+
+    }
+}
+
 The number of nodes in the list is in the range [1, 105].
 0 <= Node.val <= 9
