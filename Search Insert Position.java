1eee32c66b5781bb7e7af4ac0c3fd3f1db5a0bf5
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-08-15 11:24:21 +0530

    Update Search Insert Position.java

--- Search Insert Position.java
+++ Search Insert Position.java
@@ -35,3 +35,20 @@ nums contains distinct values sorted in ascending order.
 Solution-
 
 
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        int mid=i+(j-i)/2;
+        while(i<=j){
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]<target)
+                i=mid+1;
+            else if(nums[mid]>target)
+                j=mid-1;
+            mid=i+(j-i)/2;
+        }
+        return i;
+    }
+}
