f718035d607b5775df07c70b3568e9760aa1db7e
Gaurang Agrawal <99584477+Gaurang1602@users.noreply.github.com>
2023-08-17 21:06:52 +0530

    Update Course Schedule.java

--- Course Schedule.java
+++ Course Schedule.java
@@ -33,3 +33,48 @@ All the pairs prerequisites[i] are unique.
 
 
 Solution-
+
+
+ class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+            for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+            for (int[] pr : prerequisites) {
+                 int course = pr[0];
+                 int prerequisite = pr[1];
+                 graph.get(course).add(prerequisite);
+        }
+
+            int[] visited = new int[numCourses]; // 0 - not visited, 1 - visiting, 2 - visited
+
+        for (int course = 0; course < numCourses; course++) {
+                if (hasCycle(course, visited, graph)) {
+                    return false;
+                }
+        }
+
+            return true;
+    }
+
+    private boolean hasCycle(int course, int[] visited, List<List<Integer>> graph) {
+         if (visited[course] == 1) {
+                return true;
+            }
+            if (visited[course] == 2) {
+                return false;
+            }
+
+        visited[course] = 1;
+        for (int prerequisite : graph.get(course)) {
+            if (hasCycle(prerequisite, visited, graph)) {
+                return true;
+            }
+        }
+
+        visited[course] = 2;
+        return false;
+    }
+}
