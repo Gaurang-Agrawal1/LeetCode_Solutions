96e5e2e2ad016b01c919cff26bf531ac662a6b2b
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2024-08-15 17:11:09 +0530

    Update Lemonade Change.java

--- Lemonade Change.java
+++ Lemonade Change.java
@@ -35,4 +35,35 @@ Constraints:
 bills[i] is either 5, 10, or 20.
 
 
-  
+  Solution-
+
+  class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int fives = 0, tens = 0;
+        
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;  
+            } else if (bill == 10) {
+                if (fives > 0) {
+                    fives--; 
+                    tens++;   
+                } else {
+                    return false;  
+                }
+            } else { 
+                if (tens > 0 && fives > 0) {
+                    tens--;  
+                    fives--; 
+                } else if (fives >= 3) {
+                    fives -= 3;  
+                } else {
+                    return false; 
+                }
+            }
+        }
+        
+        return true;
+    }
+}
+
