dd7f6d39644617ed99aa6f9bc44a9f1cd7bec469
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2025-03-06 22:50:20 +0530

    Update Find Missing and Repeated Values.java

--- Find Missing and Repeated Values.java
+++ Find Missing and Repeated Values.java
@@ -30,3 +30,28 @@ For all x that 1 <= x <= n * n except two of them there is exatly one pair of i,
 Solution-
 
 
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int size = n * n;
+        int[] count = new int[size + 1];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                count[grid[i][j]]++;
+            }
+        }
+
+        int a = -1, b = -1;
+
+        for (int num = 1; num <= size; num++) {
+            if (count[num] == 2) {
+                a = num;
+            } else if (count[num] == 0) {
+                b = num;
+            }
+        }
+
+        return new int[]{a, b};
+    }
+}
