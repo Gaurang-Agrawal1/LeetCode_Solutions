1c72f166abe0d901450b118c3f9a29218fd3358e
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2025-05-11 22:37:10 +0530

    Update Three Consecutive Odds.java

--- Three Consecutive Odds.java
+++ Three Consecutive Odds.java
@@ -23,3 +23,17 @@ Constraints:
 Solution-
 
 
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int x : arr) {
+            if ((x & 1) == 1) {
+                count++;
+                if (count == 3) return true;
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+}
