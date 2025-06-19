abfb8ec7ae95463b83564068146a5a83ff9e23a4
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2025-06-19 22:28:31 +0530

    Update Partition Array Such That Maximum Difference Is K.java

--- Partition Array Such That Maximum Difference Is K.java
+++ Partition Array Such That Maximum Difference Is K.java
@@ -43,3 +43,21 @@ Constraints:
 1 <= nums.length <= 105
 0 <= nums[i] <= 105
 0 <= k <= 105
+
+
+Solution-
+
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = 1;
+        int min = nums[0];
+        for (int i = 1;  i < nums.length; i++) {
+            if (nums[i] - min > k) {
+                ans++;
+                min = nums[i];
+            }
+        }
+        return ans;
+    }
+}
