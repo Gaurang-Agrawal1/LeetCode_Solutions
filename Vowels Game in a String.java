c6982c7bc4d014f3c475ccce91932d39f1f41728
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2025-09-12 21:56:21 +0530

    Update Vowels Game in a String.java

--- Vowels Game in a String.java
+++ Vowels Game in a String.java
@@ -46,4 +46,13 @@ s consists only of lowercase English letters.
 Solution-
 
 
-
+class Solution {
+    public boolean doesAliceWin(String s) {
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (c=='a' || c=='e' || c=='i' || c=='o' || c=='u')
+                return true;
+        }
+        return false;
+    }
+}
