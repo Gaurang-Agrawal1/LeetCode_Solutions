b74419cf1b6d25e660fad6b408fcbf8ba03dfbde
Gaurang Agrawal <99584477+Gaurang-Agrawal1@users.noreply.github.com>
2026-02-24 22:48:27 +0530

    Add sumRootToLeaf method with DFS implementation
    Implement depth-first search to calculate the sum of binary numbers represented by root-to-leaf paths.

--- Sum of Root To Leaf Binary Numbers.java
+++ Sum of Root To Leaf Binary Numbers.java
@@ -26,3 +26,28 @@ Constraints:
 
 The number of nodes in the tree is in the range [1, 1000].
 Node.val is 0 or 1.
+
+
+Solution-
+
+
+class Solution {
+    
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+    
+    private int dfs(TreeNode node, int current) {
+        if (node == null) {
+            return 0;
+        }
+        
+        current = current * 2 + node.val;
+        
+        if (node.left == null && node.right == null) {
+            return current;
+        }
+        
+        return dfs(node.left, current) + dfs(node.right, current);
+    }
+}
